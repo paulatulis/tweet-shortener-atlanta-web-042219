9e1bbc3a473afd8980caac1a5d65ab4482173953
Paula Tulis <tulis.paula@gmail.com>
2019-04-13 20:59:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -14,4 +14,3 @@ def dictionary
 }
 end 
 
-tweet_string = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
